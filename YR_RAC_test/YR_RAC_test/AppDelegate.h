f371f8d0fbfe3c501991c26e105a638ee8b434db
YongRen <zhanxin13@gmail.com>
2016-02-25 15:06:03 +0800

    create demo

---
+++ YR_RAC_test/YR_RAC_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YR_RAC_test
+//
+//  Created by YongRen on 16/2/25.
+//  Copyright © 2016年 YongRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YR_RAC_test/YR_RAC_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YR_RAC_test
+//
+//  Created by YongRen on 16/2/25.
+//  Copyright © 2016年 YongRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
