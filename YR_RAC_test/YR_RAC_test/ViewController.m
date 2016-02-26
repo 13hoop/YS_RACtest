7aa8d040cd2361c527ccdb09870fb0648f01ddca
YongRen <zhanxin13@gmail.com>
2016-02-26 15:54:48 +0800

    Library not loaded: @rpath/libswiftCore.dylib... Reason:image not found❌错误处理： 设置EMBEDDED_CONTENT_CONTAINS_SWIFT build setting to “Yes”

--- YR_RAC_test/YR_RAC_test/ViewController.m
+++ YR_RAC_test/YR_RAC_test/ViewController.m
@@ -11,6 +11,10 @@
 
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+
+@property (assign, nonatomic) BOOL isStatusHidden;
 
 @end
 
@@ -18,14 +22,73 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     
+    //    [self test001];
+    
+    [self test002];
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)logInBtnClick:(id)sender {
+    
 }
 
+- (void) test002 {
+    
+    // map 去的只能说对象，所以bool要包装成NSNumber
+    RACSignal *validUserNameSG = [self.userNameTF.rac_textSignal map:^id(NSString *text) {
+        return @([self isValidUserName: text]);
+    }];
+    RACSignal *validPasswordSG = [self.passwordTF.rac_textSignal map:^id(NSString *text) {
+        return @([self isValidxPassword: text]);
+    }];
+    
+    // 将validUserNameSG信号输出应用到输入框的backgroundColor上
+    [[validUserNameSG map:^id(NSNumber *userNameValid) {
+        return [userNameValid boolValue]? [UIColor clearColor] : [UIColor yellowColor];
+    }] subscribeNext:^(UIColor *color) {
+        self.userNameTF.backgroundColor = color;
+    }];
+    // 更优雅的写法，利用RAC宏 - 直接把信号对应输出到对象的属性上
+        RAC(self.passwordTF, backgroundColor) =
+        [validPasswordSG map:^id(NSNumber *passwordValid) {
+            return [passwordValid boolValue]? [UIColor clearColor] : [UIColor yellowColor];
+        }];
+}
+
+- (void) test001 {
+    // signal流给订阅者－subscriber
+    [self.userNameTF.rac_textSignal subscribeNext:^(NSString *text) {
+        NSLog(@"  --userName Input:  %@", text);
+    }];
+    
+    // signal流过滤，会产生过滤后的signal－filter
+    [[self.passwordTF.rac_textSignal filter:^BOOL(NSString *text) {
+        return text.length > 6;
+    }] subscribeNext:^(NSString *text) {
+        NSLog(@"  --password Input:  %@", text);
+    }];
+    
+    // 对源signal流的更改，产生对不同值变化的新signal － map － signal的内容为对象
+    // －－－NSString－－－［map］－－－> NSNumber（NSUInteger封装成对象）－－－
+    [[[self.passwordTF.rac_textSignal map:^id(NSString *text) {
+        return @(text.length);
+    }] filter:^BOOL(NSNumber *length) {
+        return [length integerValue] > 4;
+    }] subscribeNext:^(NSNumber *length) {
+        NSLog(@" - 当前PWD长度： %@", length);
+    }];
+    
+}
+
+/**
+ *  用户名 > 3
+ *  密码 > 4
+ */
+- (BOOL) isValidUserName:(NSString *)text {
+    return text.length > 3;
+}
+- (BOOL) isValidxPassword:(NSString *)text {
+    return text.length > 4;
+}
 @end
